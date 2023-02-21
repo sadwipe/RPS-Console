62ed5ab6c35b70f491bccf2bb10a67d75c19d5c7
sadwipe <sadwipe@gmail.com>
2023-02-21 17:22:07 +0200

    HTML&CSS done, and first part of Javascript.

--- script.js
+++ script.js
@@ -1,67 +1,18 @@
-const button = document.querySelector(".play");
-const CHOICES = ["rock", "paper", "scissors"];
-const defaultMessage = "Choose between rock, paper and scissors!";
-const wrongMessage = "You misspelled a word, try again!";
-let playerPoints = 0, computerPoints = 0, ties = 0;
+const startButton = document.querySelector(".start");
+const preview = document.querySelector(".preview")
+const container = document.querySelector(".container");
+const choices = document.querySelectorAll(".choice");
 
-function getComputerChoice() {
-    return (CHOICES[Math.floor(Math.random() * 3)]);
-}
+
+startButton.addEventListener('click', () => {
+    container.style.cssText = "opacity: 1; pointer-events: all; transition: all 0.5s ease;";
+    preview.style.cssText = "opacity: 0; pointer-events: none;";
+});
 
 function getPlayerChoice() {
-    let playerChoice = prompt(defaultMessage);
-    if(playerChoice == null || playerChoice == "") {
-        let playerChoice = prompt(defaultMessage);
-    }
-    playerChoice = playerChoice.toLowerCase();
-    let check = validateInput(playerChoice);
-    if(check) {
-        return playerChoice;
-    } else {
-        while(!check) {
-            alert(wrongMessage);
-            playerChoice = prompt(defaultMessage);
-            check = validateInput(playerChoice);
-        }
-        return playerChoice;
-    }
+    choices.forEach(element => element.addEventListener('click', (e) => {
+        return e.target.value;
+    }))
 }
 
-function validateInput(input) {
-    return CHOICES.includes(input);
-}
 
-function playRound(player, computer) { // plays a round
-    console.log(`You choosed: ${player}`);
-    console.log(`Computer choosed: ${computer}`);
-    if (player == "rock" && computer == "scissors" || 
-    player == "paper" && computer == "rock" || 
-    player == "scissors" && computer == "paper") {
-        console.log("You won!");
-        console.log("----------------------")
-        playerPoints++;
-    } else if (player == computer) {
-        console.log("It's a tie!");
-        console.log("----------------------")
-        ties++;
-    } else {
-        console.log("Computer won!");
-        console.log("----------------------")
-        computerPoints++;
-    }
-}
-
-function game() { // plays the game 5 times and displays the score.
-    for (let i = 0; i < 5; i++) {
-        playRound(getPlayerChoice(), getComputerChoice());
-    }
-    console.log('********** SCORE **********')
-    console.log(`Player: ${playerPoints}`);
-    console.log(`Computer: ${computerPoints}`);
-    console.log(`Ties: ${ties}`);
-    playerPoints = 0;
-    computerPoints = 0;
-    ties = 0;
-}
-
-button.addEventListener('click', game);
